90043f9203e884031ea766bc6a0f672eb0edb262
Abhik Chakraborty <105542546+Abhik-Chakraborty@users.noreply.github.com>
2025-02-28 23:24:33 +0530

    Time: 0 ms (100%), Space: 11.8 MB (5.04%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -7,7 +7,7 @@ public:
         
         int i = 0;  
         
-        for (int j = 0; j < n; ++j) {
+        for (int j = 0; j < n; j++) {
             if (nums[j] != val) {
                 nums[i] = nums[j];  
                 ++i;
