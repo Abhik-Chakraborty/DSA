44b5a0614cb7de602d180e6bdd80f5c5c43a7884
Abhik Chakraborty <105542546+Abhik-Chakraborty@users.noreply.github.com>
2025-03-03 12:08:40 +0530

    Time: 0 ms (100%), Space: 11.9 MB (53.22%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -4,21 +4,15 @@ public:
         if(strs.empty()){
             return "";
         }
+        sort(strs.begin(), strs.end());
 
-        for(int i = 0; i < strs[0].size(); i++){
-            char c = strs[0][i];
+        string first = strs[0];
+        string last = strs[strs.size() - 1];
 
-
-            for(int j = 1; j < strs.size(); j++){
-                if(i >= strs[j].size() || strs[j][i] != c){
-                    return strs[0].substr(0, i);
-                }
-            }
+        int i = 0; 
+        while(i < first.size() && i < last.size() && first[i] == last[i]){
+            i++;
         }
-        return strs[0];
-
-        
-
-    }   
-
+        return first.substr(0, i);
+    }
 };
