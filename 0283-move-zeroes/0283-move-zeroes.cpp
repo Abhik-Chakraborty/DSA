4a06b8bd66ff9ef5db950a5baef077111c89342b
Abhik Chakraborty <105542546+Abhik-Chakraborty@users.noreply.github.com>
2025-03-07 19:05:16 +0530

    Time: 0 ms (100%), Space: 24.6 MB (7.21%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int size = nums.size();
-        int j = -1;
-
-        //find the first zero in the given array 
-        for(int i = 0; i < size; i++){
-            if(nums[i] == 0){
-                j = i; // j store the index of i
-                break;
-            }
-        }
-
-        if(j == -1) return;//no zeros is found, no need to return anything 
-
-        //i pointer will start from next of j, if i is having non element then it will swap wth j
-        for(int i = j + 1; i < size; i++){
+        vector<int> temp;
+        for(int i = 0; i < nums.size(); i++){
             if(nums[i] != 0){
-                swap(nums[i], nums[j]);
-                j++;
+                temp.push_back(nums[i]); //[1, 3, 12]
             }
         }
-        
+        int tempSize = temp.size();
+        for(int i = 0; i < tempSize; i++){
+            nums[i] = temp[i];
+        }
+        for(int i = tempSize; i < nums.size(); i++){
+            nums[i] = 0;
+        }
+
     }
 };
