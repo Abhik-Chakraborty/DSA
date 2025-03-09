f86b2d2ffadef15d4e610e4b50fcd7b265492928
Abhik Chakraborty <105542546+Abhik-Chakraborty@users.noreply.github.com>
2025-03-09 23:44:22 +0530

    Time: 0 ms (100%), Space: 10.8 MB (45.88%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        vector<string> words;
         string temp;
-
+        vector<string> words;
         for(int i = 0; i < s.size(); i++){
             if(s[i] != ' '){
                 temp += s[i];
@@ -16,10 +15,8 @@ public:
             }
         }
         if(!temp.empty()){
-            words.push_back(temp);
+            words.push_back(temp); // words = the,sky,is,blue
         }
-
-        //reverse
         string result;
         for(int i = words.size() - 1; i >= 0; i--){
             result += words[i];
@@ -30,6 +27,5 @@ public:
 
         }
         return result;
-        
     }
 };
