3e7c1429c64afc5a0d351c9d637c2f09416c71e2
Abhik Chakraborty <105542546+Abhik-Chakraborty@users.noreply.github.com>
2025-03-09 19:40:00 +0530

    Time: 4 ms (54.04%), Space: 127.4 MB (25.08%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -11,22 +11,20 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        ListNode* temp = head;
         stack<int> st;
-
+        ListNode* temp = head;
         while(temp != NULL){
-            st.push(temp->val);
+            st.push(temp -> val);
             temp = temp->next;
         }
         temp = head;
         while(temp != NULL){
-            if(temp->val != st.top()){
+            if(temp -> val != st.top()){
                 return false;
             }
+            temp = temp -> next;
             st.pop();
-            temp = temp->next;
         }
         return true;
-        
     }
 };
