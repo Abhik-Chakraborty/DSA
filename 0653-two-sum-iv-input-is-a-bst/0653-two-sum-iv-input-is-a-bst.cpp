05a183a68498293a13622d9a53d836abcf39d54f
Abhik Chakraborty <105542546+Abhik-Chakraborty@users.noreply.github.com>
2025-03-25 11:36:45 +0530

    Time: 0 ms (100%), Space: 37.2 MB (58.68%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -22,16 +22,20 @@ public:
     bool findTarget(TreeNode* root, int k) {
         vector<int> arr;
         inOrder(root, arr); // we get the sorted array here. 
-        for(int i = 0; i < arr.size(); i++){
-            int sum = 0;
-            for(int j = i + 1; j < arr.size(); j++){
-                sum = arr[i] + arr[j];
-                if(sum == k){
-                    return true;
-                }
+        int i = 0;
+        int j = arr.size() - 1;
+        while(i < j){
+            int sum = arr[i] + arr[j];
+            if(sum > k){
+                j--;
+            }
+            else if (sum < k){
+                i++;
+            }
+            else if (sum == k){
+                return true;
             }
         }
         return false;
-
     }
 };
