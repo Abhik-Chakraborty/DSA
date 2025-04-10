43cdd39419d97080846edcb6f627e85b96879b1d
Abhik Chakraborty <105542546+Abhik-Chakraborty@users.noreply.github.com>
2025-04-10 12:36:00 +0530

    Time: 5 ms (81.99%), Space: 44.9 MB (43.49%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -5,16 +5,14 @@ public:
         sort(s.begin(), s.end());
         int left = 0;
         int right = 0;
-        int count = 0;
         while(left < s.size() && right < g.size()){
             if(s[left] >= g[right]){
-                count++;
                 left++;
                 right++;
             }else{
                 left++;
             }
         }
-        return count;
+        return right;
     }
 };
