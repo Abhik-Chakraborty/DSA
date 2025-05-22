9c8e3d8cde0c9dfb265667682a6eccd177c19d84
Abhik Chakraborty <105542546+Abhik-Chakraborty@users.noreply.github.com>
2025-05-22 20:02:22 +0530

    Time: 0 ms (100%), Space: 29.9 MB (33.5%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,19 +3,16 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k % n;
-        
-        //putting k element to the temp array
-        vector<int>temp;
-        for(int i = n - k; i < n; i++){
-            temp.push_back(nums[i]);
+
+        vector<int> temp(k);
+        for(int i = 0; i < k; i++){
+            temp[i] = nums[n - k + i];
         }
 
-        //shifting
-        for(int i = n-1; i >= k; i--){
+        for(int i = n - 1; i >= k; i--){
             nums[i] = nums[i - k];
         }
 
-        //putting all back 
         for(int i = 0; i < k; i++){
             nums[i] = temp[i];
         }
