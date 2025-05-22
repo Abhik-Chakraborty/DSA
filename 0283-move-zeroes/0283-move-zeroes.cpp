de903960bcab8babe60f894e7c50fff6cd33dc8d
Abhik Chakraborty <105542546+Abhik-Chakraborty@users.noreply.github.com>
2025-05-22 21:02:40 +0530

    Time: 0 ms (100%), Space: 24.8 MB (5.83%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = -1;
-        //trying to get zero and point it to j pointer.
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] == 0){
-                j = i;
-                break;
-            }
-        }
-        if(j == -1) return;
-        // I got j's pos
-        for(int i = j + 1; i < nums.size(); i++){
-            if(nums[i] != 0){
-                swap(nums[i], nums[j]);
-                j++;
-            }
-        }
+        vector<int> nonZeros;
 
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != 0){
+                nonZeros.push_back(nums[i]);
+            }
+        }
+        for(int i = 0; i < nonZeros.size(); i++){
+            nums[i] = nonZeros[i];
+        }
+        for(int i = nonZeros.size(); i < nums.size(); i++){
+            nums[i] = 0;
+        }
     }
 };
